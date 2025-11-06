13bccced0c3d5751a764bdd231db752374ace7fd
Peter <lrxicon2025@proton.me>
2025-11-06 15:12:57 +0100

    android

---
+++ HTML/Project2/android/app/src/main/java/com/example/todoapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.todoapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true); // Enable localStorage
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Load the todo app from assets
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
